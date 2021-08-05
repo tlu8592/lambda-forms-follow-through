6963fb2a5c4150954fd3db8ea970f4e0f7d86655
Tony Lu <70731161+tlu8592@users.noreply.github.com>
2021-08-05 17:22:59 -0400

    added onChange handler on input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [name, setName] = useState();
+
   return (
     <div className="App">
+      {console.log(name)}
       <form>
         <label>
           Username:
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
         </label>
       </form>
     </div>
