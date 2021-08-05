2c8414f86267af2bdb069de5fbcbec67e01db936
Tony Lu <70731161+tlu8592@users.noreply.github.com>
2021-08-05 17:26:51 -0400

    declared handleChange function somewhere else Therefore, many lines of code will be executed as desired without cluttering up the markup

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import "./styles.css";
 export default function App() {
   const [name, setName] = useState();
 
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div className="App">
       {console.log(name)}
       <form>
         <label>
           Username:
-          <input
-            type="text"
-            onChange={(event) => setName(event.target.value)}
-          />
+          <input type="text" onChange={(event) => handleChange(event)} />
         </label>
       </form>
     </div>
