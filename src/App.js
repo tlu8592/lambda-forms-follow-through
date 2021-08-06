3f68a020757f7e988228fe8597501e4de534d40b
Tony Lu <70731161+tlu8592@users.noreply.github.com>
2021-08-05 21:15:52 -0400

    added password input

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import "./styles.css";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleChange = (event) => {
     setName(event.target.value);
   };
 
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(name);
+    console.log(password);
   };
 
   return (
     <div className="App">
-      {console.log(name)}
+      {console.log({ name })}
+      {console.log({ password })}
       <form onSubmit={(event) => handleSubmit(event)}>
         <label>
           Username:
           <input type="text" onChange={(event) => handleChange(event)} />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            onChange={(event) => handlePasswordChange(event)}
+          />
         </label>
         <button>Submit!</button>
       </form>
